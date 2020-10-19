dfa0e53460084228e7ecd4baf60bcd82e33a0c03
Wilson <wilsonj806@gmail.com>
2020-10-19 13:02:03 -0400

    Implement hamburger nav for desktop

---
+++ scripts/nav.js
@@ -0,0 +1,20 @@
+const hamburger = document.getElementById('navBurger')
+const close = document.getElementById('close')
+
+hamburger.addEventListener('click', () => {
+  const navMenu = document.getElementById('navMenu')
+  const open = navMenu.getAttribute('data-open')
+  console.log(open)
+
+  if (open == 'false') {
+    navMenu.classList.add('nav__menu--active')
+    navMenu.setAttribute('open','true')
+  }
+})
+
+close.addEventListener('click', () => {
+  const navMenu = document.getElementById('navMenu')
+  navMenu.classList.remove('nav__menu--active')
+  navMenu.setAttribute('open','false')
+
+})
